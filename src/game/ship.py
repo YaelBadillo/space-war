76429fc92d0804314b263909ad0d642e785ea138
Yael Badillo <yael.badillos@gmail.com>
2023-08-03 00:16:16 -0600

    Refactor: refactor keys when press and release

--- src/game/ship.py
+++ src/game/ship.py
@@ -7,27 +7,37 @@ class Ship(physicalobject.PhysicalObject):
         super(Ship, self).__init__(*args, **kwargs)
 
         self.rotation = -90
-        self.velocity_x = 100
-        self.velocity_y = 100
+        self.velocity_x = 300
+        self.velocity_y = 300
 
         self.keys = dict(up=False, down=False, left=False, right=False)
 
     def on_key_press(self, symbol, modifiers):
-        if symbol == key.A:
+        if symbol == key.W:
             self.keys['up'] = True
         if symbol == key.S:
             self.keys['down'] = True
         if symbol == key.A:
             self.keys['left'] = True
-        if symbol == key.A:
+        if symbol == key.D:
             self.keys['right'] = True
 
     def on_key_release(self, symbol, modifiers):
-        if symbol == key.A:
+        if symbol == key.W:
             self.keys['up'] = False
         if symbol == key.S:
             self.keys['down'] = False
         if symbol == key.A:
             self.keys['left'] = False
-        if symbol == key.A:
+        if symbol == key.D:
             self.keys['right'] = False
+
+    def update(self, dt):
+        if self.keys['up']:
+            self.y += self.velocity_y * dt
+        if self.keys['down']:
+            self.y -= self.velocity_y * dt
+        if self.keys['left']:
+            self.x -= self.velocity_x * dt
+        if self.keys['right']:
+            self.x += self.velocity_x * dt
