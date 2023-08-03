195e6ffbc18600c994c7f4d853a9530a19e36454
Yael Badillo <yael.badillos@gmail.com>
2023-08-03 00:02:24 -0600

    Feat: set fighter image center

--- src/index.py
+++ src/index.py
@@ -5,8 +5,11 @@ from game import ship, resources
 window = pyglet.window.Window(width=800, height=600)
 batch = pyglet.graphics.Batch()
 
+fighter_image = resources.fighter_image
+fighter_image.anchor_x = fighter_image.width // 2
+fighter_image.anchor_y = fighter_image.height // 2
 fighter = ship.Ship(img=resources.fighter_image, batch=batch,
-                    x=window.width / 2, y=window.height / 2)
+                    x=window.width / 2, y=(window.height / 2) - 150)
 
 @window.event
 def on_draw():
@@ -16,18 +19,16 @@ def on_draw():
 
 @window.event
 def on_key_press(symbol, modifiers):
-    # fighter.on_key_press(symbol, modifiers)
-    pass
+    fighter.on_key_press(symbol, modifiers)
 
 
 @window.event
 def on_key_release(symbol, modifiers):
-    # fighter.on_key_release(symbol, modifiers)
-    pass
+    fighter.on_key_release(symbol, modifiers)
 
 
 def update(dt):
-    pass
+    fighter.update(dt)
 
 
 pyglet.clock.schedule_interval(update, 1 / 60.0)
