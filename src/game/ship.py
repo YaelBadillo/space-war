83663cd820d349a4e2dab5a69d78b3e97c6a0606
Yael Badillo <yael.badillos@gmail.com>
2023-08-04 00:08:21 -0600

    Feat: add check bounds method

--- src/game/ship.py
+++ src/game/ship.py
@@ -3,9 +3,10 @@ from . import physicalobject
 
 
 class Ship(physicalobject.PhysicalObject):
-    def __init__(self, *args, **kwargs):
-        super(Ship, self).__init__(*args, **kwargs)
+    def __init__(self, img, *args, **kwargs):
+        super(Ship, self).__init__(img=img, *args, **kwargs)
 
+        self.img = img
         self.rotation = -90
         self.velocity_x = 200
         self.velocity_y = 200
@@ -42,3 +43,20 @@ class Ship(physicalobject.PhysicalObject):
             self.x -= self.velocity_x * dt
         if self.keys['right']:
             self.x += self.velocity_x * dt
+        
+        self.__check_bounds()
+
+    def __check_bounds(self):
+        min_x = -self.img.width / 2
+        min_y = -self.img.height / 2
+        max_x = 1000 + self.img.width / 2
+        max_y = 750 + self.img.height / 2
+        if self.x < min_x:
+            self.x = max_x
+        if self.y < min_y:
+            self.y = max_y
+        if self.x > max_x:
+            self.x = min_x
+        if self.y > max_y:
+            self.y = min_y
+
